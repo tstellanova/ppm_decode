7e5dd776293109b3371aa0d5861b3fc941bd2b63
Todd Stellanova <tstellanova@users.noreply.github.com>
2020-05-21 15:26:45 -0700

    Fix pulse parsing, add tests, add comments

--- README.md
+++ README.md
@@ -1,2 +1,35 @@
 # ppm_decode
 PPM decoding for embedded hal , no_std rust
+
+
+## Example
+
+Typically PPM input might be received via an input pin interrupt.
+The important thing is that you provide this parser with the time
+of the start of a pulse. In PPM the only time difference that 
+matters is the difference between consecutive pulses. 
+
+```rust
+    let mut parser = PpmParser::new();   
+    let mut cur_time: PpmTime = 100;
+ 
+    loop {
+        parser.handle_pulse_start(cur_time);    
+        if let Some(frame) = parser.next_frame() {
+            //TODO process the frame
+        }
+
+        cur_time += 100; //TODO get from clock, interrupt, or whatever
+    }   
+``` 
+
+## Status
+
+- [x] Basic parsing of anonymous PPM  time events
+- [x] Basic tests
+- [x] Usage example
+- [ ] Test clock overflow
+- [ ] Test unusual configuration limits
+- [ ] Locks onto a consistent number of channels over time
+- [ ] Doc comments
+- [ ] CI
--- src/lib.rs
+++ src/lib.rs
@@ -1,8 +1,9 @@
+/*
+Copyright (c) 2020 Todd Stellanova
+LICENSE: BSD3 (see LICENSE file)
+*/
+
 #![no_std]
-
-
-use crate::PpmPhase::PULSE_ACTIVE;
-use core::borrow::Borrow;
 
 /// Base type for PPM timing
 pub type Microseconds = u32;
@@ -10,17 +11,16 @@ pub type Microseconds = u32;
 /// PPM timing values
 pub type PpmTime = Microseconds;
 
-/// Default minimum channel value
-pub const MIN_CHAN_VAL: PpmTime = 1000; //TODO s/b 800?
-/// Default maximum channel value
-pub const MAX_CHAN_VAL: PpmTime = 2000; //TODO s/b 2200 ?
+/// Default minimum channel value:
+/// in practice as low as 800 microseconds
+pub const MIN_CHAN_VAL: PpmTime = 1000;
+/// Default maximum channel value:
+/// in practice typically up to 2200 microseconds
+pub const MAX_CHAN_VAL: PpmTime = 2000;
 /// Default midpoint channel value
 pub const MID_CHAN_VAL: PpmTime = (MAX_CHAN_VAL + MIN_CHAN_VAL) / 2;
 
-/// Default minimum width of a pulse
-pub const MIN_PULSE_WIDTH: PpmTime =   200;
-
-/// Default minimum reset/sync gap
+/// Default minimum gap between frames (no pulses / inactive)
 pub const MIN_SYNC_WIDTH: PpmTime = 2300;
 
 /// Default minimum number of channels per frame
@@ -28,7 +28,6 @@ pub const MIN_PPM_CHANNELS: u8 = 5;
 
 /// theoretical maximum PPM channels in normal usage
 pub const MAX_PPM_CHANNELS: usize = 20;
-
 
 /// Errors in this crate
 #[derive(Debug)]
@@ -45,15 +44,8 @@ pub struct PpmFrame {
     chan_count: u8,
 }
 
-enum PpmPhase {
-    /// we have not yet received a long frame synchronization gap
-    UNSYNC,
-    /// we've received the start of a pulse
-    PULSE_ACTIVE,
-}
-
-pub struct PpmParser {
-
+#[derive(Copy, Clone, Debug)]
+pub struct ParserConfig {
     /// Configurable minimum channel value
     min_chan_value: PpmTime,
 
@@ -65,9 +57,116 @@ pub struct PpmParser {
 
     /// Configurable minimum number of channels per valid frame
     min_channels: u8,
+}
 
-    /// Current parsing phase
-    phase: PpmPhase,
+impl Default for ParserConfig {
+    fn default() -> Self {
+        Self {
+            min_chan_value: MIN_CHAN_VAL,
+            max_chan_value: MAX_CHAN_VAL,
+            min_sync_width: MIN_SYNC_WIDTH,
+            min_channels: MIN_PPM_CHANNELS
+        }
+    }
+}
+
+
+impl PpmParser {
+    pub fn new() -> Self {
+        Self {
+            config: Default::default(),
+            working_frame: PpmFrame {
+                chan_values: [0; MAX_PPM_CHANNELS],
+                chan_count: 0,
+            },
+            parsed_frame: None,
+            state: ParserState::Scanning,
+            last_pulse_start: 0,
+        }
+    }
+
+    /// Configure channel value range
+    pub fn set_channel_limits(
+        &mut self,
+        min: PpmTime,
+        max: PpmTime,
+    ) -> &mut Self {
+        self.config.min_chan_value = min;
+        self.config.max_chan_value = max;
+        self
+    }
+
+    /// Configure duration of frame sync
+    pub fn set_sync_width(&mut self, width: PpmTime) -> &mut Self {
+        self.config.min_sync_width = width;
+        self
+    }
+
+    /// Handle a pulse start
+    pub fn handle_pulse_start(&mut self, count: PpmTime) {
+        let width = count.wrapping_sub(self.last_pulse_start);
+        self.last_pulse_start = count;
+
+        match self.state {
+            ParserState::Scanning => {
+                // assume we've never received any pulses before:
+                // detect a long sync/reset gap
+                if width >= self.config.min_sync_width {
+                    //received sync
+                    self.reset_channel_counter();
+                    self.state = ParserState::Synced;
+                }
+            }
+            ParserState::Synced => {
+                // previous pulse has ended
+                if width >= MIN_SYNC_WIDTH {
+                    //received sync -- we should be finished with prior frame
+                    //TODO verify we receive a consistent number of channels
+                    if self.working_frame.chan_count >= self.config.min_channels {
+                        self.parsed_frame.replace(self.working_frame);
+                    } else {
+                        //we didn't get expected minimum number of channels
+                        self.parsed_frame = None;
+                    }
+                    self.reset_channel_counter();
+                }
+                else {
+                    // verify the pulse received is within limits, otherwise resync
+                    if width >= self.config.min_chan_value  &&
+                        width <= self.config.max_chan_value {
+                        self.working_frame.chan_values
+                            [self.working_frame.chan_count as usize] = width;
+                        self.working_frame.chan_count += 1;
+                        //TODO verify we haven't received TOO MANY channels (<MAX_PPM_CHANNELS)
+                    } else {
+                        // bogus pulse -- resynchronize
+                        self.reset_channel_counter();
+                        self.state = ParserState::Scanning;
+                    }
+                }
+            }
+        }
+    }
+
+    /// Get the next available PPM frame, if any
+    pub fn next_frame(&mut self) -> Option<PpmFrame> {
+        self.parsed_frame.take()
+    }
+
+    /// We've either finished receiving all channels
+    /// (and have received a sync/reset)
+    /// or we received garbage and need to clear our buffers.
+    fn reset_channel_counter(&mut self) {
+        self.working_frame.chan_count = 0;
+    }
+}
+
+pub struct PpmParser {
+    /// Parser configuration
+    config: ParserConfig,
+
+    /// Current parsing state
+    state: ParserState,
 
     /// the last time an (active) pulse started
     last_pulse_start: PpmTime,
@@ -77,90 +176,13 @@ pub struct PpmParser {
 
     /// frame ready for consumption
     parsed_frame: Option<PpmFrame>,
-
 }
 
-impl PpmParser {
-    pub fn new() -> Self {
-        Self {
-            min_chan_value: MIN_CHAN_VAL,
-            max_chan_value: MAX_CHAN_VAL,
-            working_frame: PpmFrame {
-                chan_values: [0; MAX_PPM_CHANNELS],
-                chan_count: 0,
-            },
-            parsed_frame: None,
-            min_sync_width: MIN_SYNC_WIDTH,
-            min_channels: MIN_PPM_CHANNELS,
-            phase: PpmPhase::UNSYNC,
-            last_pulse_start: 0,
-        }
-    }
-
-    /// Configure channel value range
-    pub fn set_channel_limits(&mut self, min: PpmTime, max: PpmTime) -> &mut Self {
-        self.min_chan_value = min;
-        self.max_chan_value = max;
-        self
-    }
-
-    /// Configure duration of frame sync
-    pub fn set_sync_width(&mut self, width: PpmTime) -> &mut Self {
-        self.min_sync_width = width;
-        self
-    }
-
-    /// Handle a pulse start
-    pub fn handle_pulse_start(&mut self, count: PpmTime) {
-        let width = count - self.last_pulse_start;
-        self.last_pulse_start = count;
-
-        match self.phase {
-            PpmPhase::UNSYNC => {
-                // assume we've never received any pulses before
-                if width >= MIN_SYNC_WIDTH {
-                    //received sync
-                    self.reset_channel_counter();
-                    self.phase = PpmPhase::PULSE_ACTIVE;
-                }
-            }
-            PpmPhase::PULSE_ACTIVE => {
-                // previous pulse has ended
-                if width >= MIN_SYNC_WIDTH {
-                    self.working_frame.chan_count += 1;
-                    //received sync -- we should be finished with prior frame
-                    //TODO verify we receive a consistent number of channels
-                    if self.working_frame.chan_count >= self.min_channels {
-                        self.parsed_frame.replace(self.working_frame);
-                    }
-                    else {
-                        //we didn't get expected minimum number of channels
-                        self.parsed_frame = None;
-                    }
-                    self.reset_channel_counter();
-                }
-                else {
-                    self.working_frame.chan_values[self.working_frame.chan_count as usize] = width;
-                    self.working_frame.chan_count += 1;
-                }
-            }
-        }
-
-    }
-
-
-    pub fn next_frame(&mut self) -> Option<PpmFrame> {
-        self.parsed_frame.take()
-    }
-
-    /// We've either finished receiving all channels
-    /// (and have received a sync/reset)
-    /// or we received garbage and need to clear our buffers.
-    fn reset_channel_counter(&mut self) {
-
-
-        self.working_frame.chan_count = 0;
-    }
+enum ParserState {
+    /// we have not yet received a long reset/synchronization
+    Scanning,
+    /// we've received a sync and are trying to receive pulses
+    Synced,
 }
 
 #[cfg(test)]
@@ -172,8 +194,9 @@ mod tests {
         const TEST_CHAN_COUNT: u8 = 16;
         const TEST_RESYNC_WIDTH: PpmTime = 2500;
         let mut parser = PpmParser::new();
-        parser.set_channel_limits(800, 2200)
-            .set_sync_width(TEST_RESYNC_WIDTH);
+        parser
+            .set_channel_limits(800, 2200)
+            .set_sync_width(TEST_RESYNC_WIDTH - 10);
 
         let mut cur_time: PpmTime = 100;
         //start with a garbage pulse from prior packet
@@ -184,7 +207,7 @@ mod tests {
         assert!(frame.is_none(), "there should be no frame yet");
 
         //send a full frame
-        for _ in 0..TEST_CHAN_COUNT {
+        for _ in 0..TEST_CHAN_COUNT + 1 {
             parser.handle_pulse_start(cur_time);
             let frame = parser.next_frame();
             assert!(frame.is_none(), "frame should be incomplete");
@@ -200,7 +223,10 @@ mod tests {
 
         if let Some(frame) = frame_opt {
             let valid_chans = frame.chan_count as usize;
-            assert_eq!(valid_chans, TEST_CHAN_COUNT as usize, "wrong number of channels");
+            assert_eq!(
+                valid_chans, TEST_CHAN_COUNT as usize,
+                "wrong number of channels"
+            );
             for i in 0..valid_chans {
                 let val = frame.chan_values[i];
                 assert_eq!(val, MID_CHAN_VAL)
